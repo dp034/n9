f7063cef5d4724d52a70298f8dc29d2fe94be157
light <117119346+dp034@users.noreply.github.com>
2022-11-03 07:26:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
         mm = String(today.getMonth() + 1).padStart(2, "0"),
         yyyy = today.getFullYear(),
         nextYear = yyyy + 1,
-        dayMonth = "11/02/",
+        dayMonth = "11/03/",
         birthday = dayMonth + yyyy ;
     
     today = mm + "/" + dd + "/" + yyyy;
